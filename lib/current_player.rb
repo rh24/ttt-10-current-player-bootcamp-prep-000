e8be3c8bdb30f74eb00f163d2ca327d0327a68bf
Rebecca Hong <ms.rebeccahong@gmail.com>
2017-10-04 15:55:11 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -3,11 +3,11 @@ index = 0
   # until index = board.length-1
     turns = 0
     board.each do |turn|
-    if board[index] != " "
-      turns += 1
-      index += 1
+      if board[index] != " "
+        turns += 1
+        index += 1
+      end
     end
-  end
 end
 
 def current_player
